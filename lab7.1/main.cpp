139d055f664b3019df3447aa837b9e802c03094b
Andrew Petrov <marsofandrew@gmail.com>
2020-09-18 23:08:28 +0300

    Finished lab 7.1

--- lab7.1/main.cpp
+++ lab7.1/main.cpp
@@ -3,24 +3,30 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
-#define NMAX 11
+#define NMAX 16
 
 double a[NMAX];
 
 
-int main(void) {
+int main(int argc, char* argv[]) {
 
     a[0] = 1;
     double f = 2.0;
 # pragma omp parallel for
     for (int i = 1; i < NMAX; i++) {
-        a[i] = f * a[i - 1];
+        //a[i] = f * a[i - 1]; wouldn't be parallel because of data race
+        a[i] = std::pow(f, i);
     }
 
+    for (int i = 0; i < NMAX; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "\n";
     return 0;
 }
