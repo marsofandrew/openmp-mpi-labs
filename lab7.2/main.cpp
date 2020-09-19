59b85fa2bcc0e21823c6b0d1ef56ce2db515677c
Andrew Petrov <marsofandrew@gmail.com>
2020-09-19 15:02:31 +0300

    Implemented task 2 in lab 7.2

--- lab7.2/main.cpp
+++ lab7.2/main.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <vector>
+#include <omp.h>
 
 using Matrix = std::vector<std::vector<double>>;
 
@@ -25,11 +26,14 @@ Matrix createSquareMatrix(size_t size) {
 }
 
 Matrix multiply(const Matrix &a, const Matrix &b) {
+    omp_set_nested(1);
     int size = a.size();
     Matrix matrix = createSquareMatrix(size);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            for (int k = 0; k < size; k++) {
+    for (int k = 0; k < size; k++) {
+#pragma omp parallel for
+        for (int i = 0; i < size; i++) {
+#pragma omp parallel for
+            for (int j = 0; j < size; j++) {
                 matrix[i][j] += a[i][k] * b[k][j];
             }
         }
@@ -38,6 +42,15 @@ Matrix multiply(const Matrix &a, const Matrix &b) {
 }
 
 int main(int argc, char *argv[]) {
+    omp_set_dynamic(0);
+    int threads_num = 4;
+
+    if (argc >= 2) {
+        threads_num = std::stoi(argv[1]);
+    }
+    std::cout << "Threads = " << threads_num << "\n";
+    omp_set_num_threads(threads_num);
+
     Matrix a = {{0, 1, 2},
                 {3, 4, 5},
                 {6, 7, 8}};
