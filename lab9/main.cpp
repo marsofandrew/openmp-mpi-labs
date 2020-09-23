21d265bbdd704be06f3a48c9575728a84b43355e
Andrew Petrov <marsofandrew@gmail.com>
2020-09-24 01:06:03 +0300

    Finished lab9

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -3,26 +3,64 @@
 //
 #include <mpi.h>
 #include <iostream>
-#define leng 20 //length of WR-string
-int main( int argc, char **argv )
-{
-    double t,t2;
-    int i,rank,size;
-    char WR[leng];
-    MPI_Status status;
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    sprintf(WR,"Hello from %d",rank); // формирование сообщения
-    t=MPI_Wtime(); // фиксация времени «начала посылки»,
-// локально для каждого процесса
-    MPI_Send(WR,leng,MPI_CHAR,size-(rank+1),rank,MPI_COMM_WORLD); // (1)
-    MPI_Recv(WR,leng,MPI_CHAR,rank,size-(rank+1),MPI_COMM_WORLD,&status); // (2)
-    t2=MPI_Wtime(); // фиксация времени «окончания приема»,
-// локально для каждого процесса73
-    printf("\n From processor %d\n WR=%s\n",rank,WR); // вывод сообщения
-    printf("\n From processor %d\n Time=%le\n",rank,(t2-t)/100); // вывод времени,
-// затраченного на обмен данным процессором
+#include <string>
+#include <vector>
+#include <random>
+#include <cmath>
+
+
+enum status_t {
+    FAIL,
+    SUCCESS
+};
+
+
+int main(int argc, char **argv) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    int rank = 0, size = 0;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size < 2) {
+        std::cout << "at least 2 processors are required to execute the program\n";
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    if (rank == 0) {
+        //John Silver process
+        std::uniform_int_distribution<int> dist(1, size);
+        int success = dist(mt);
+
+        for (int i = 1; i < size; ++i) {
+            status_t status = status_t::FAIL;
+            if (i == success) {
+                status = status_t::SUCCESS;
+            }
+            MPI_Send(&status, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        for (int j = 1; j < size; ++j) {
+            status_t status = {};
+            MPI_Recv(&status, 1, MPI_INT, j, j, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "Group " << j << ": status " << status << "\n";
+        }
+    } else {
+        status_t status = {};
+        MPI_Recv(&status, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        std::uniform_int_distribution<int> dist(1000, 1000000000);
+        double b = 0;
+        for (int i = 0; i < dist(mt); ++i) {
+            double a = std::pow(0.95, i);
+            b = a * i;
+        }
+        dist(mt);
+
+        MPI_Send(&status, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+    }
     MPI_Finalize();
     return 0;
 }
